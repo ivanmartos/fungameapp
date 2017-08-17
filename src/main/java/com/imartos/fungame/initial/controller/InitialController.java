44e10939da25d2680c17271e4f62c7c7b641dbcb
Ivan Martos <martos.ivan@gmail.com>
2017-08-17 15:20:44 +0200

    using sample service

--- src/main/java/com/imartos/fungame/initial/controller/InitialController.java
+++ src/main/java/com/imartos/fungame/initial/controller/InitialController.java
@@ -1,14 +1,23 @@
 package com.imartos.fungame.initial.controller;
 
+import com.imartos.fungame.initial.service.InitialService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InitialController {
 
+	private final InitialService initialService;
+
+	@Autowired
+	public InitialController(InitialService initialService) {
+		this.initialService = initialService;
+	}
+
 	@RequestMapping("/")
 	public String home() {
-		return "Hello world";
+		return initialService.initiate();
 	}
 
 }
--- src/test/java/com/imartos/fungame/initial/controller/InitialControllerTest.java
+++ src/test/java/com/imartos/fungame/initial/controller/InitialControllerTest.java
@@ -1,30 +1,57 @@
 package com.imartos.fungame.initial.controller;
 
+import com.imartos.fungame.initial.service.InitialService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class InitialControllerTest {
 
-	@Autowired
 	private MockMvc mvc;
+
+	@Mock
+	private InitialService anyService;
+
+	@InjectMocks
+	private InitialController initialController;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(initialController).build();
+	}
+
+	//	@Test
+	//	public void getHello() throws Exception {
+	//		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(
+	//				content().string(equalTo("Hello world")));
+	//	}
 
 	@Test
 	public void getHello() throws Exception {
+		String anyResultString = "ANY RESULT STRING";
+
+		when(anyService.initiate()).thenReturn(anyResultString);
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(
-				content().string(equalTo("Hello world")));
+				content().string(equalTo(anyResultString)));
+		verify(anyService).initiate();
 	}
+
 }
