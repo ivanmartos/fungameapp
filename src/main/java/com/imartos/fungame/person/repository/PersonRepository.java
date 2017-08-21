1a6a2844e82d08631bf401932abdd2db8347a177
Ivan Martos <martos.ivan@gmail.com>
2017-08-21 10:58:21 +0200

    added repository annotation

--- src/main/java/com/imartos/fungame/person/repository/PersonRepository.java
+++ src/main/java/com/imartos/fungame/person/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package com.imartos.fungame.person.repository;
 
 import com.imartos.fungame.person.domain.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
@@ -9,4 +10,5 @@ import javax.transaction.Transactional;
 // https://stackoverflow.com/questions/30880927/spring-boot-extending-crudrepository
 
 @Transactional
+@Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {}
